a2192f791ca3ccad95051983ba53e3edf3c448c5
IlyaKirichenko <ikirichenko345@gmail.com>
2025-04-21 11:49:31 +0500

    Add files via upload

--- Задание1.kt
+++ Задание1.kt
@@ -1,25 +1,38 @@
 import kotlinx.coroutines.*
-suspend fun pressButton (id: Int){
-    delay(500L)
-    println("Кнопка $id нажата")
-}
-suspend fun getData(id:Int){
+suspend fun pressButton (n: Int): String{
     delay(800L)
-    println("Данные $id получены")
+    return "Кнопка $n нажата"
+}
+suspend fun getData(n:Int):String{
+    delay(500L)
+    return "Данные $n получены"
 }
 suspend fun main()= coroutineScope{
     println("Введите количество функций: ")
     val n = readLine()!!.toInt()
-if(n > 0)
-{
-    for (i in 1..n) {
-        launch { getData(i) }
-    }
-    for (i in 1..n){
-        launch { pressButton(i) }
-    }
-}
-    else{
+    if(n <= 0) {
         println("n должно быть больше 0")
+    }
+    else{
+        var buttonRes =""
+        var dataRes = ""
+        for (i in 1..n) {
+            launch {
+                buttonRes += pressButton(i) + "\n"
+
+            }
+        }
+        for (i in 1..n){
+            launch {
+                dataRes += pressButton(i) + "\n"
+
+            }
+        }
+        delay(500L * n)
+        println("\nРезультаты нажатий")
+        println(buttonRes)
+        println("\nРезультаты данных")
+        println(dataRes)
+        println("\nВсего выполнено: ${n*2} операций")
     }
 }
